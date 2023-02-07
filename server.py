21babe05b1066fc1e8c8ba8c8bf2e39ef2c76584
Tim Zhou <timothy_zhou@my.uri.edu>
2023-02-07 16:16:02 -0500

    Added screenshots for each project

--- server.py
+++ server.py
@@ -13,19 +13,19 @@ def elements():
 def html_page(page_name):
     return render_template(page_name)
 
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data["email"]
-        name = data["name"]
-        message = data["message"]
-        file = database.write(f'\n{email},{name},{message}')
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data["email"]
+#         name = data["name"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{name},{message}')
 
 def write_to_csv(data):
     with open('database.csv', mode='a') as database2:
         email = data["email"]
         name = data["name"]
         message = data["message"]
-        csv_writer = csv.writer(database2, delimiter=',', newline='', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([email,name,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
